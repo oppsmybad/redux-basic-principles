ae139d971ff7d39d1994b51be169f0a4d68cb232
blesswrld <roninfortnite71@gmail.com>
2025-03-09 20:18:42 +0300

    add react in project

---
+++ src/components/Counter.jsx
@@ -0,0 +1,16 @@
+const Counter = ({ counter, inc, dec, rnd }) => {
+    <div className="jumbotron" style="text-align: center; margin-top: 50px">
+        <h1 id="counter">{counter}</h1>
+        <button onClick={inc} className="btn btn-primary">
+            INC
+        </button>
+        <button onClick={dec} className="btn btn-primary">
+            DEC
+        </button>
+        <button onClick={rnd} className="btn btn-primary">
+            RND
+        </button>
+    </div>;
+};
+
+export default Counter;
--- src/index.jsx
+++ src/index.jsx
@@ -1,51 +1,21 @@
+import React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createStore, bindActionCreators } from "redux";
+import { createStore } from "redux";
 import reducer from "./reducer";
-import * as actions from "./actions";
+import { Provider } from "react-redux";
+
+import App from "./components/App";
 
 // Хранилище
 const store = createStore(reducer);
 
-const { dispatch, subscribe, getState } = store;
-
-const update = () => {
-    document.getElementById("counter").textContent = getState().value; // записываем значения внутрь счетчика
-};
-
-// Вызываем функцию update
-subscribe(update);
-
-// Создаем общую функция для привязки actions с аргументами
-// const bindActionCreator =
-//     (creator, dispatch) =>
-//     (...args) => {
-//         dispatch(creator(...args));
-//     };
-
-// Передаем каждый action в виде ключей и привязываем к bindActionCreators
-const { inc, dec, rnd } = bindActionCreators(actions, dispatch);
-
-// const decDispatch = bindActionCreators(dec, dispatch);
-// const rndDispatch = bindActionCreators(rnd, dispatch);
-
-document.getElementById("inc").addEventListener("click", inc); // +1
-
-document.getElementById("dec").addEventListener("click", dec); // -1
-
-document.getElementById("rnd").addEventListener("click", () => {
-    const value = Math.floor(Math.random() * 10);
-    rnd(value); // random number
-});
-
-// ''
-
-// Вывод в консоль
-// let state = reducer(undefined, { type: "INC" }); // 1
-// state = reducer(state, { type: "INC" });
-// state = reducer(state, { type: "INC" });
-// state = reducer(state, { type: "INC" });
-// console.log(state); // 4
-
 const root = createRoot(document.getElementById("root"));
-root.render(<StrictMode></StrictMode>);
+
+root.render(
+    <StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </StrictMode>
+);
